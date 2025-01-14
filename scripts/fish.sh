c13df566faf8df7ea9130303ec102fbbc0823ba4
Manish Kumar <manish.kumar@mercari.com>
2025-01-14 20:20:42 +0900

    feat(repo): update config

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -4,7 +4,6 @@
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
 # Virtualfish
-python3 -m pip install virtualfish
 vf install
 
 # NVM
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,5 +13,11 @@ chmod +x ./scripts/fish.sh
 # Install Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Install tmux theme
+git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.tmux/plugins/catppuccin/tmux
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 # Setup symlinks for Config
 stow .
