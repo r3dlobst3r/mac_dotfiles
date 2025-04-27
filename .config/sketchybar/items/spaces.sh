27f2486d8d79f5c7d34e7b387c30d26687ae2aaf
Manish Kumar ⛄ <manishprivet@protonmail.com>
2025-04-27 15:37:45 +0900

    refactor(sketchybar): aerospace config

--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-arr["1"]="Calendar"
-
-arr["2"]="Ghostty"
-
-arr["3"]="Google Chrome"
-
-arr["4"]="Slack"
-
-arr["5"]="Safari"
-
-arr["W"]="WhatsApp"
-
 for sid in $(aerospace list-workspaces --all); do
   sketchybar --add item space."$sid" left \
     --subscribe space."$sid" aerospace_workspace_change \
     --set space."$sid" \
-    icon="$("$CONFIG_DIR/plugins/icon_map_fn.sh" "${arr[${sid}]}")" \
+    padding_right=0 \
+    icon="$sid" \
+    label.padding_right=7 \
     icon.padding_left=7 \
-    icon.padding_right=7 \
+    icon.padding_right=4 \
     background.drawing=on \
-    icon.font="sketchybar-app-font:Regular:16.0" \
+    label.font="sketchybar-app-font:Regular:16.0" \
     background.color="$ACCENT_COLOR" \
     icon.color="$BACKGROUND" \
     label.color="$BACKGROUND" \
     background.corner_radius=5 \
     background.height=25 \
-    label.drawing=off \
+    label.drawing=on \
     click_script="aerospace workspace $sid" \
     script="$CONFIG_DIR/plugins/aerospace.sh $sid"
 done
@@ -35,6 +25,6 @@ sketchybar --add item space_separator left \
   --set space_separator icon="|" \
   icon.color="$ACCENT_COLOR" \
   icon.padding_left=4 \
-  icon.padding_right=15 \
+  icon.padding_right=7 \
   label.drawing=off \
   background.drawing=off
--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -5,12 +5,32 @@
 source "$CONFIG_DIR/colors.sh"
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set "$NAME" --animate tanh 30 background.drawing=on \
+  sketchybar --set "$NAME" background.drawing=on \
     background.color="$ACCENT_COLOR" \
     label.color="$BACKGROUND" \
     icon.color="$BACKGROUND"
 else
-  sketchybar --set "$NAME" --animate tanh 30 background.drawing=off \
+  sketchybar --set "$NAME" background.drawing=off \
     label.color="$ACCENT_COLOR" \
-    icon.color="$ACCENT_COLOR"
+    icon.color="$ACCENT_COLOR" \
+    background.color="$BAR_COLOR"
+fi
+
+icons=""
+
+IFS=$'\n'
+for sid in $(aerospace list-windows --workspace "$1" --json | jq -r "map ( .\"app-name\" ) | .[]"); do
+  icons+=$("$CONFIG_DIR/plugins/icon_map_fn.sh" "$sid")
+  icons+="  "
+done
+
+# When icons is empty, set it to " "
+if [ -z "$icons" ]; then
+  if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+    sketchybar --set "$NAME" label="$icons" drawing=on
+  else
+    sketchybar --set "$NAME" drawing=off
+  fi
+else
+  sketchybar --set "$NAME" label="$icons" drawing=on
 fi
