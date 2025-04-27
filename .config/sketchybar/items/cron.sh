970dbfcf05d714965b2ab0f6ee240023a37009ea
Manish Kumar <manish.kumar@mercari.com>
2025-04-28 03:59:45 +0900

    chore(sketchybar): add 'display=1' property for cron and music bar items
    - .config/sketchybar/items/cron.sh: Added 'display=1' for consistency and visibility control.
    - .config/sketchybar/items/music.sh: Set 'display=1' for both music and music_artwork items to standardize appearance.

--- .config/sketchybar/items/cron.sh
+++ .config/sketchybar/items/cron.sh
@@ -3,6 +3,7 @@
 sketchybar --add item cron right \
   --set cron icon=󰢠 \
   label="..." \
+  display=1 \
   update_freq=120 \
   icon.font="sketchybar-app-font:Regular:16.0" \
   script="$PLUGIN_DIR/cron.js"
