ee2421967d04e27848947c807d6690b84039e5c2
Manish Kumar <manish.kumar@mercari.com>
2025-04-28 09:58:50 +0900

    Enhance volume widget: - Add update_freq to periodic updates - Improve plugin to support BetterDisplay for volume & mute reading - Refactor: safer bash, fallback icons, and value normalizing

--- .config/sketchybar/plugins/volume.sh
+++ .config/sketchybar/plugins/volume.sh
@@ -1,20 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
 # The volume_change event supplies a $INFO variable in which the current volume
 # percentage is passed to the script.
+VOLUME="0"
 
 if [ "$SENDER" = "volume_change" ]; then
   VOLUME=$INFO
 
-  case $VOLUME in
-    [6-9][0-9]|100) ICON="􀊩"
-    ;;
-    [3-5][0-9]) ICON="􀊥"
-    ;;
-    [1-9]|[1-2][0-9]) ICON="􀊡"
-    ;;
-    *) ICON="􀊣"
-  esac
+elif command -v betterdisplaycli 2>&1 >/dev/null; then
+  ALTERNATE_VOLUME=$(betterdisplaycli get -feature=volume -value -displayWithMainStatus | bc)
 
-  sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
+  MEGA=100
+
+  if [[ $ALTERNATE_VOLUME =~ "." ]]; then
+    VOLUME=$(bc -l <<<"${ALTERNATE_VOLUME}*${MEGA}" | bc)
+    VOLUME=$(echo "$VOLUME" | cut -d. -f1)
+  fi
+
+  MUTE=$(betterdisplaycli get -feature=mute -value -displayWithMainStatus)
+
+  if [ "$MUTE" = "on" ]; then
+    VOLUME="0"
+  fi
 fi
+
+case $VOLUME in
+[6-9][0-9] | 100)
+  ICON="􀊩"
+  ;;
+[3-5][0-9])
+  ICON="􀊥"
+  ;;
+[1-9] | [1-2][0-9])
+  ICON="􀊡"
+  ;;
+*) ICON="􀊣" ;;
+esac
+
+sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"
