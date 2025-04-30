4c265e350b5fe81ab61dcda3a7ec46c3c5f73b4c
Manish Kumar ⛄ <manish.kumar@mercari.com>
2025-04-30 09:29:30 +0900

    chore: 🧹 update fish function and git config

--- .config/fish/functions/aicommit.fish
+++ .config/fish/functions/aicommit.fish
@@ -1,4 +1,4 @@
 function aicommit --wraps='ai --full-auto "generate a commit message and commit it"' --description 'alias aicommit=ai --full-auto "generate a commit message and commit it"'
-  ai --full-auto "generate a commit message and commit it" $argv
+  ai --full-auto "generate a commit message and commit it. Take the commit message inspiration from /Users/manishprivet/.config/git/template" $argv
         
 end
